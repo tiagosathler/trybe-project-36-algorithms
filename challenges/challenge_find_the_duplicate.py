26a0aeed5dc04cd82f34a3c2cf98133434c5003c
Tiago <sathler@gmail.com>
2022-07-27 15:34:42 -0300

    :sparkles: feat[challenge_find_the_duplicate.py]: implementa a função 'find_duplicate'

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,48 @@
-def find_duplicate(nums):
-    """ Faça o código aqui. """
+def find_duplicate(nums: list):
+    """
+    Analisa uma lista não ordenada de inteiros e verifica se há pelo
+    menos um repetido
+
+    Entradas:
+    ---------
+
+    nums : list[int]
+        uma lista de números inteiros não ordenados
+
+    Saída:
+    ------
+
+    int | bool
+        False:
+            se a lista é vazia
+            ou se seus itens não são inteiros
+            ou se seus itens não inteiros positivos
+
+        int: o primeiro número inteiro encontrado em duplicidade
+    """
+
+    if not(
+        len(nums) > 1
+        and
+        all([isinstance(number, int) for number in nums])
+        and
+        all([number >= 0 for number in nums])
+    ):
+        return False
+
+    nums.sort()
+
+    start = 0
+    end = len(nums) - 1
+
+    while start <= end:
+        mid = (start + end) // 2
+        previous_number = nums[mid-1]
+        if nums[mid] == previous_number:
+            return previous_number
+        if nums[mid] > previous_number:
+            start = mid + 1
+        else:
+            end = mid - 1
+
+    return False
