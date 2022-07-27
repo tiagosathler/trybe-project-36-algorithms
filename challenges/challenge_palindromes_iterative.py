c9860ef7cfc6e555a066ac4311804a728714541b
Tiago <sathler@gmail.com>
2022-07-27 16:52:45 -0300

    :sparkles: feat[challenge_palindrome_iterative.py]: implementa a função 'is_palindrome_iterative'

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,32 @@
-def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+def is_palindrome_iterative(word: str) -> bool:
+    """
+    Analisa uma palavra se é palíndroma ou não usando um
+    algoritmo iterativo.
+
+    Entradas:
+    ---------
+
+    word : string
+        uma palavra a ser analisada (case insensitive)
+
+
+    Saída:
+    ------
+
+    True | False:
+        a resposta se é palíndroma
+        False caso a palavra for vazia
+    """
+
+    if not word:
+        return False
+
+    length_of_word = len(word)
+    mid = length_of_word // 2
+
+    for index in range(length_of_word):
+        if (index < mid - length_of_word % 2):
+            if (word[index] != word[0 - index - 1]):
+                return False
+        else:
+            return True
